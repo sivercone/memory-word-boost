4cb7b3eb75ffb56f0b3779be892bb25acd2cf099
Danylo Trofimenko <49727045+sivercone@users.noreply.github.com>
2021-12-02 01:00:32 +0100

    set delete api endpoint connected

--- client/src/api/setApi.ts
+++ client/src/api/setApi.ts
@@ -5,13 +5,13 @@ const path = 'http://localhost:7001/set';
 export const setApi = {
    async getSets(): Promise<SetInterface[]> {
       const response = await fetch(path);
-      if (!response.ok) throw new Error('Something went wrong');
+      if (!response.ok) throw new Error(response.statusText);
       return response.json();
    },
 
    async getSetById(payload: string): Promise<SetInterface> {
       const response = await fetch(`${path}/${payload}`);
-      if (!response.ok) throw new Error('Something went wrong');
+      if (!response.ok) throw new Error(response.statusText);
       return response.json();
    },
 
@@ -21,6 +21,11 @@ export const setApi = {
          headers: { 'Content-Type': 'application/json' },
          body: JSON.stringify(payload),
       });
-      if (!response.ok) throw new Error('Something went wrong');
+      if (!response.ok) throw new Error(response.statusText);
+   },
+
+   async deleteSet(payload: string): Promise<void> {
+      const response = await fetch(`${path}/${payload}`, { method: 'DELETE' });
+      if (!response.ok) throw new Error(response.statusText);
    },
 };
--- client/src/pages/[set].tsx
+++ client/src/pages/[set].tsx
@@ -1,12 +1,24 @@
 import type { NextPage } from 'next';
 import style from 'styles/pages/set.module.scss';
 import Link from 'next/link';
-import { QueryClient, useQuery, dehydrate } from 'react-query';
+import { QueryClient, useQuery, dehydrate, useMutation } from 'react-query';
 import { setApi } from 'api/setApi';
+import { useRouter } from 'next/dist/client/router';
+import { notify } from 'utils/notify';
 
 const SetPage: NextPage<{ pagekey?: string }> = ({ pagekey }) => {
    const set = useQuery(['set', pagekey], () => setApi.getSetById(pagekey!), { enabled: !!pagekey });
    if (!set.data) return <p>no data</p>;
+   const router = useRouter();
+
+   const { mutateAsync } = useMutation(setApi.deleteSet);
+   const onDeleteSet = async () => {
+      try {
+         await mutateAsync(set.data._id);
+         router.push('/');
+         notify(`Successfully deleted Study Set #${pagekey}`);
+      } catch (error) {}
+   };
 
    return (
       <div className="container">
@@ -67,7 +79,7 @@ const SetPage: NextPage<{ pagekey?: string }> = ({ pagekey }) => {
                      <path d="M14.06 9.02l.92.92L5.92 19H5v-.92l9.06-9.06M17.66 3c-.25 0-.51.1-.7.29l-1.83 1.83 3.75 3.75 1.83-1.83c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.2-.2-.45-.29-.71-.29zm-3.6 3.19L3 17.25V21h3.75L17.81 9.94l-3.75-3.75z" />
                   </svg>
                </button>
-               <button title="delete">
+               <button onClick={onDeleteSet} title="delete">
                   <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24" fill="#181818">
                      <path d="M0 0h24v24H0V0z" fill="none" />
                      <path d="M16 9v10H8V9h8m-1.5-6h-5l-1 1H5v2h14V4h-3.5l-1-1zM18 7H6v12c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7z" />
