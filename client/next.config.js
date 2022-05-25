db898a5308d1d05f8bc08da342c327b24c249718
Danylo Trofimenko <49727045+sivercone@users.noreply.github.com>
2022-05-26 00:00:18 +0200

    fix(next.config): disabled pwa

--- client/next.config.js
+++ client/next.config.js
@@ -8,6 +8,7 @@ const withPWA = require('next-pwa');
 module.exports = withPWA({
   pwa: {
     dest: 'public',
-    disable: process.env.NODE_ENV === 'development',
+    disable: true,
+    // disable: process.env.NODE_ENV === 'development',
   },
 });
