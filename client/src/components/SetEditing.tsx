d2b4ce8fc8ecd93433928fef004a0237522d0ecd
Danylo Trofimenko <49727045+sivercone@users.noreply.github.com>
2022-09-05 15:15:35 +0200

    style(client): improvements with look

--- client/src/components/SetEditing.tsx
+++ client/src/components/SetEditing.tsx
@@ -72,7 +72,7 @@ const SetEditing: NextPage<{ setFigure?: SetFigure }> = ({ setFigure }) => {
               <input type="text" placeholder="term" {...register(`cards.${i}.term`)} required />
               <input type="text" placeholder="definition" {...register(`cards.${i}.definition`)} required />
               <button onClick={() => remove(i)} type="button" title="delete">
-                <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24" fill="#cccccc">
+                <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24" fill="currentColor">
                   <path d="M0 0h24v24H0V0z" fill="none" />
                   <path d="M16 9v10H8V9h8m-1.5-6h-5l-1 1H5v2h14V4h-3.5l-1-1zM18 7H6v12c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7z" />
                 </svg>
