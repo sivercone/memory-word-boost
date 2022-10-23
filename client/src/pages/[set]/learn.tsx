a8507f140e7b73e4995dfb3033dba8eca720ef4b
Danylo Trofimenko <49727045+sivercone@users.noreply.github.com>
2022-10-23 18:05:08 +0200

    feat: base logic for new learn method

--- client/src/pages/[set]/learn.tsx
+++ client/src/pages/[set]/learn.tsx
@@ -1,89 +1,74 @@
 import React from 'react';
 import { NextPage } from 'next';
-import { useForm } from 'react-hook-form';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 import { dehydrate, QueryClient, useQuery } from 'react-query';
 import { setApi } from 'apis/setApi';
-import Custom404 from 'pages/404';
-import style from 'styles/pages/learn.module.scss';
-import { useRouter } from 'next/router';
-import { Button } from 'ui/Button';
 import { CardInterface } from 'interfaces';
-import Link from 'next/link';
+import style from 'styles/pages/learn.module.scss';
+import { motion } from 'framer-motion';
+import { motions } from './flashcards';
+import Custom404 from 'pages/404';
 
-type Results = {
-  round: number;
-  incorrectCards: CardInterface[];
-  correctCards: CardInterface[];
-};
-type SubmitData = { answer: string };
+type StudyCard = CardInterface & { flash: boolean; write: boolean; quiz: boolean };
 
 const LearnPage: NextPage<{ pagekey: string }> = ({ pagekey }) => {
   const { push } = useRouter();
   const set = useQuery(['set', pagekey], () => setApi.getById(pagekey));
 
-  const [cards, setCards] = React.useState<CardInterface[]>([]);
+  const [cards, setCards] = React.useState<StudyCard[]>([]);
   React.useEffect(() => {
-    if (set.data) setCards(set.data.cards.sort(() => Math.random() - 0.5));
-  }, [set.data]);
-  const [currentIndex, setCurrentIndex] = React.useState<number>(0);
-  const scorePercent = Math.round((currentIndex / cards.length) * 100);
-  const [status, setStatus] = React.useState<'T' | 'F' | 'E'>('T');
-  const [round, setRound] = React.useState<number>(1);
-  const [incorrect, setIncorrect] = React.useState<CardInterface[]>([]);
-
-  const loseCard = () => {
-    setStatus('F');
-    if (!incorrect.find((el) => el === cards[currentIndex])) {
-      setIncorrect((prev) => [...prev, cards[currentIndex]]);
+    if (set.data) {
+      const studyCards = set.data.cards.map((card) => ({ ...card, flash: false, write: false, quiz: false }));
+      setCards(studyCards.sort(() => Math.random() - 0.5));
     }
+  }, [set.data]);
+
+  const [currentIndex, setCurrentIndex] = React.useState<number>(0);
+  const [cardsLength, scorePercent] = React.useMemo(() => {
+    const cardsLength = cards.length * 3;
+    const scorePercent = Math.round((currentIndex / cardsLength) * 100);
+    return [cardsLength, scorePercent];
+  }, [currentIndex, cards.length]);
+
+  const [toggled, setToggled] = React.useState(false);
+  const [isToggling, setIsToggling] = React.useState(false);
+  const onToggle = () => {
+    if (currentIndex >= cardsLength || cards[currentIndex].flash) return;
+    setIsToggling(true);
+    setToggled(!toggled);
+    setTimeout(() => setIsToggling(false), 300);
   };
 
-  const nextCard = () => {
-    if (currentIndex + 1 >= cards.length) {
-      setStatus('E');
-      setResults((prev) => [...prev, { round: round, incorrectCards: incorrect, correctCards: cards }]);
-    } else {
-      setStatus('T');
+  const onFlash = () => {
+    const studyCards: StudyCard[] = JSON.parse(JSON.stringify(cards));
+    if (!studyCards[currentIndex].flash) studyCards[currentIndex].flash = true;
+    // else if (studyCards[currentIndex].flash && !studyCards[currentIndex].quiz) studyCards[currentIndex].quiz = true;
+    // else studyCards[currentIndex].write = true;
+    setCards(studyCards);
+    if (currentIndex + 1 >= cards.length) setCurrentIndex(0);
+    else setCurrentIndex((prev) => prev + 1);
+    setToggled(false);
+  };
+
+  const onQuiz = (answer: string) => {
+    if (cards[currentIndex].definition === answer) {
+      const studyCards: StudyCard[] = JSON.parse(JSON.stringify(cards));
+      studyCards[currentIndex].quiz = true;
+      setCards(studyCards);
       setCurrentIndex((prev) => prev + 1);
     }
-    reset();
   };
 
-  const { register, handleSubmit, reset } = useForm<SubmitData>();
-  const onSubmit = (payload: SubmitData) => {
-    if (
-      payload.answer
-        .toLowerCase()
-        .replace(/^\s+|\s+$/g, '')
-        .replace(/\s+/g, ' ') === cards[currentIndex].definition.toLowerCase()
-    )
-      nextCard();
-    else loseCard();
-  };
+  const quizItems = React.useMemo(() => {
+    const studyCards = cards
+      .sort(() => Math.random() - 0.5)
+      .filter((c) => c.term + c.definition !== cards[currentIndex].term + cards[currentIndex].definition)
+      .slice(0, 3);
+    return [...studyCards, cards[currentIndex]].sort(() => Math.random() - 0.5);
+  }, [currentIndex, cardsLength]);
 
-  const [results, setResults] = React.useState<Results[]>([]);
-
-  const nextRound = () => {
-    setCurrentIndex(0);
-    setCards(incorrect);
-    setIncorrect([]);
-    setRound((prev) => prev + 1);
-    setStatus('T');
-  };
-
-  const restartRound = () => {
-    if (!set.data) return;
-    setIncorrect([]);
-    setCards(set.data.cards.sort(() => Math.random() - 0.5));
-    setCurrentIndex(0);
-    setRound(1);
-    setResults([]);
-    setStatus('T');
-  };
-
-  // TODO
-  // 1. add fade animations for changing text in blocks
-  // 2. add diffrent emojis, dependent on completing, for result header
+  console.log(cards);
 
   if (!set.data) return <Custom404 />;
   return (
@@ -101,106 +86,105 @@ const LearnPage: NextPage<{ pagekey: string }> = ({ pagekey }) => {
           </Link>
         </div>
       </header>
-      {cards.length && status !== 'E' ? (
-        <div className={style.form}>
-          <div className={style.form__inner}>
-            <div className={style.form__progressbar} title={`${scorePercent}%`}>
-              <div style={{ width: `${scorePercent}%` }}></div>
-            </div>
-            <div className={style.form__main}>
-              <div className={style.form__learn}>
-                <span>{cards[currentIndex].term}</span>
-                {status === 'F' ? (
-                  <>
-                    <span>correct answer</span>
-                    <span>{cards[currentIndex].definition}</span>
-                  </>
-                ) : undefined}
-              </div>
-              <form onSubmit={handleSubmit(onSubmit)} className={style.form__fields} autoComplete="off">
-                <input type="text" {...register('answer')} required autoFocus />
-                <div>
-                  <Button type="submit">ANSWER</Button>
-                  <Button onClick={loseCard} type="button" title="click if don't know">
-                    ?
-                  </Button>
-                </div>
-              </form>
-            </div>
-          </div>
+      <div className={style.learn}>
+        <div className={style.progressbar}>
+          <span>{`${currentIndex >= cardsLength ? currentIndex : currentIndex + 1}/${cardsLength}`}</span>
+          <div style={{ width: `${scorePercent}%` }}></div>
         </div>
-      ) : undefined}
-      {status === 'E' ? (
-        <div className={style.block}>
-          {incorrect.length ? (
-            <div className={style.block__inner}>
-              <header className={style.results__header}>
-                <p>Results of Round {round}</p>
-                <p>Good, you&#39;re in progress</p>
-              </header>
-              <div className={style.results__content}>
-                <p style={{ color: 'green' }}>
-                  <span>Correct</span>
-                  <span>{cards.length - incorrect.length}</span>
-                </p>
-                <p style={{ color: 'tomato' }}>
-                  <span>Incorrect</span>
-                  <span>{incorrect.length}</span>
-                </p>
-                <p>
-                  <span>Overall progress</span>
-                  <span>{`${set.data.cards.length - incorrect.length}/${set.data.cards.length}`}</span>
-                </p>
-              </div>
-              <div className={style.results__actions}>
-                <Button onClick={() => push(`/${pagekey}`)} variant="outlined">
-                  Return to set page
-                </Button>
-                <Button onClick={nextRound} variant="outlined">
-                  Continue
-                </Button>
-              </div>
-            </div>
-          ) : (
-            <div className={style.block__inner}>
-              <header className={style.results__header}>
-                <p>Congrats!</p>
-                <p>{`You've studied ${set.data.cards.length} cards.`}</p>
-              </header>
-              <div className={style.results__content} style={{ textAlign: 'center' }}>
-                <div style={{ fontSize: '5rem' }}>🎯</div>
-              </div>
-              <div className={style.results__actions}>
-                <Button onClick={restartRound} variant="outlined">
-                  Study again
-                </Button>
-                <Button onClick={() => push(`/${pagekey}`)} variant="outlined">
-                  Return to set page
-                </Button>
-              </div>
-            </div>
-          )}
-          {!incorrect.length
-            ? results.map((el, i) => (
-                <div key={i} className={style.block__inner}>
-                  <header className={style.results__header}>
-                    <p>{`Round ${el.round}`}</p>
-                  </header>
-                  <div className={style.results__content}>
-                    {el.correctCards.map((card, i) => (
-                      <p key={i} style={el.incorrectCards.includes(card) ? { color: 'tomato' } : { color: 'green' }}>
-                        <span>
-                          {el.incorrectCards.includes(card) ? '✕' : '✓'} {card.term}
-                        </span>
-                        <span>{card.definition}</span>
-                      </p>
-                    ))}
-                  </div>
-                </div>
-              ))
-            : undefined}
+        <motion.div
+          className={style.learn__card}
+          // eslint-disable-next-line no-constant-condition
+          // animate={!'learned' ? motions.translateLeft : !'toRepeated' ? motions.translateRight : motions.init}
+        >
+          <motion.button
+            onClick={onToggle}
+            animate={toggled ? motions.rotate : motions.init}
+            disabled={currentIndex >= cardsLength || isToggling}
+          >
+            <motion.span animate={toggled ? motions.rotate : motions.init}>
+              {!isToggling ? (toggled ? cards[currentIndex]?.definition : cards[currentIndex]?.term) : ''}
+              {currentIndex >= cardsLength ? (
+                <>
+                  <p>⚡️</p>
+                  <p>Nice progress</p>
+                  <p>{`You just studied ${cardsLength} terms!`}</p>
+                </>
+              ) : undefined}
+            </motion.span>
+          </motion.button>
+        </motion.div>
+        <div className={style.learn__moves}>
+          {currentIndex >= cardsLength ? (
+            <>
+              <button className={style.learn__arrow}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16">
+                  <path d="M11.534 7h3.932a.25.25 0 0 1 .192.41l-1.966 2.36a.25.25 0 0 1-.384 0l-1.966-2.36a.25.25 0 0 1 .192-.41zm-11 2h3.932a.25.25 0 0 0 .192-.41L2.692 6.23a.25.25 0 0 0-.384 0L.342 8.59A.25.25 0 0 0 .534 9z" />
+                  <path
+                    fillRule="evenodd"
+                    d="M8 3c-1.552 0-2.94.707-3.857 1.818a.5.5 0 1 1-.771-.636A6.002 6.002 0 0 1 13.917 7H12.9A5.002 5.002 0 0 0 8 3zM3.1 9a5.002 5.002 0 0 0 8.757 2.182.5.5 0 1 1 .771.636A6.002 6.002 0 0 1 2.083 9H3.1z"
+                  />
+                </svg>
+                <span>Restart</span>
+              </button>
+              <button onClick={() => push(`/${pagekey}`)} className={style.learn__arrow}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  enableBackground="new 0 0 24 24"
+                  height="1em"
+                  viewBox="0 0 24 24"
+                  width="1em"
+                  fill="currentColor"
+                >
+                  <rect fill="none" height="24" width="24" />
+                  <path d="M15,5l-1.41,1.41L18.17,11H2V13h16.17l-4.59,4.59L15,19l7-7L15,5z" />
+                </svg>
+                <span>Return to set page</span>
+              </button>
+            </>
+          ) : !cards[currentIndex].flash ? (
+            <>
+              <button className={style.learn__arrow}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  enableBackground="new 0 0 24 24"
+                  height="1em"
+                  viewBox="0 0 24 24"
+                  width="1em"
+                  fill="currentColor"
+                >
+                  <rect fill="none" height="24" width="24" />
+                  <path d="M9,19l1.41-1.41L5.83,13H22V11H5.83l4.59-4.59L9,5l-7,7L9,19z" />
+                </svg>
+                <span>Already know</span>
+              </button>
+              <button onClick={onFlash} className={style.learn__arrow}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  enableBackground="new 0 0 24 24"
+                  height="1em"
+                  viewBox="0 0 24 24"
+                  width="1em"
+                  fill="currentColor"
+                >
+                  <rect fill="none" height="24" width="24" />
+                  <path d="M15,5l-1.41,1.41L18.17,11H2V13h16.17l-4.59,4.59L15,19l7-7L15,5z" />
+                </svg>
+                <span>Got it</span>
+              </button>
+            </>
+          ) : !cards[currentIndex].quiz ? (
+            quizItems.map((card, i) => (
+              <button
+                onClick={() => onQuiz(card.definition)}
+                key={card.term + card.definition + i}
+                className={`${style.learn__arrow} ${style.learn__arrowright}`}
+              >
+                <span>{card.definition}</span>
+              </button>
+            ))
+          ) : undefined}
         </div>
-      ) : undefined}
+      </div>
     </>
   );
 };
