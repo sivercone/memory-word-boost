baf0b567f4fa14ed516a1905571ea29c643d5b55
Danylo Trofimenko <49727045+sivercone@users.noreply.github.com>
2023-09-23 16:58:07 +0200

    refactor: user

--- client/src/modules/user/UserDetails.tsx
+++ client/src/modules/user/UserDetails.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import { NextPage } from 'next';
-import { dehydrate, QueryClient, useQuery } from 'react-query';
-import { authApi, folderApi } from '@src/apis';
-import Custom404 from '@src/pages/404';
+import { useQuery } from 'react-query';
+import { folderApi } from '@src/apis';
 import { formatDate } from '@src/lib/utils';
 import { ActionList } from '@src/ui/ActionList';
 import ProfileForm from '@src/modules/user/ProfileForm';
-import { FolderInterface, UserInterface } from '@src/interfaces';
+import { UserInterface } from '@src/interfaces';
 import { Spinner } from '@src/ui/Spinner';
 
 const UserProfile: React.FC<{ user: UserInterface; onEdit: () => void }> = ({ user, onEdit }) => (
@@ -26,45 +25,36 @@ const UserProfile: React.FC<{ user: UserInterface; onEdit: () => void }> = ({ us
   </div>
 );
 
-const UserFolders: React.FC<{ folders: FolderInterface[] }> = ({ folders }) => (
-  <div className="max-w-3xl mx-auto p-4">
-    <h2 className="text-2xl mb-4">Folders</h2>
-    <ActionList
-      data={folders}
-      keyExtractor={(item) => item.id}
-      renderItem={(item, index) => (
-        <ActionList.Link href={`/sets?folder=${item.id}`} isFirst={index === 0}>
-          <a>{item.name}</a>
-        </ActionList.Link>
-      )}
-    />
-  </div>
-);
+const UserFolders: React.FC<{ userId: string }> = ({ userId }) => {
+  const { data = [], isLoading } = useQuery(['folders', userId], () => folderApi.getByUser(userId), { enabled: !!userId });
 
-const UserDetails: NextPage<{ queryId: string }> = ({ queryId }) => {
+  if (isLoading) return <Spinner className="mx-auto m-8 h-8" />;
+  return (
+    <div className="max-w-3xl mx-auto p-4">
+      <h2 className="text-2xl mb-4">Folders</h2>
+      <ActionList
+        data={data}
+        keyExtractor={(item) => item.id}
+        renderItem={(item, index) => (
+          <ActionList.Link href={`/sets?folder=${item.id}`} isFirst={index === 0}>
+            <a>{item.name}</a>
+          </ActionList.Link>
+        )}
+      />
+    </div>
+  );
+};
+
+const UserDetails: NextPage<{ queryId: string; data: UserInterface }> = ({ data: user }) => {
   const [edit, setEdit] = React.useState(false);
-  const { data: user, isLoading: userLoading } = useQuery(['user', queryId], () => authApi.getById(queryId));
-  const { data: folders = [], isLoading: folderLoading } = useQuery(['folders', queryId], () => user && folderApi.getByUser(user.id), {
-    enabled: !!user,
-  });
-
-  if (userLoading || folderLoading) return <Spinner className="mx-auto m-8 h-8" />;
-  if (!user) return <Custom404 />;
 
   return (
     <>
       <UserProfile user={user} onEdit={() => setEdit(true)} />
       <ProfileForm open={edit} setOpen={setEdit} />
-      <UserFolders folders={folders} />
+      <UserFolders userId={user.id} />
     </>
   );
-};
-
-UserDetails.getInitialProps = async ({ query }) => {
-  const queryId = typeof query.id === 'string' ? query.id : '';
-  const queryClient = new QueryClient();
-  if (queryId) await queryClient.prefetchQuery(['user', queryId], () => authApi.getById(queryId));
-  return { queryId, dehydratedState: dehydrate(queryClient) };
 };
 
 export default UserDetails;
