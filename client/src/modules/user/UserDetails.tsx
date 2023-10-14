b88f4102a940ffd7a911cf0ba069b27ed352b1fb
Danylo Trofimenko <49727045+sivercone@users.noreply.github.com>
2023-10-14 20:14:21 +0200

    feat(UserDetails): add placeholder image

--- client/src/modules/user/UserDetails.tsx
+++ client/src/modules/user/UserDetails.tsx
@@ -11,12 +11,15 @@ import { Spinner } from '@src/ui/Spinner';
 const UserProfile: React.FC<{ user: UserInterface; onEdit: () => void }> = ({ user, onEdit }) => (
   <div className="bg-white py-8 border-b border-b-gray-200">
     <div className="max-w-3xl mx-auto flex flex-col gap-4 px-4">
-      <div>
-        <h1 className="text-2xl font-medium">{user.name}</h1>
-        <p className="leading-relaxed text-gray-600 text-sm">
-          On project since {formatDate({ createdAt: user.createdAt, pattern: 'dd MMM yyyy' })}
-        </p>
-        {user.bio ? <p className="leading-relaxed text-gray-600">{user.bio}</p> : null}
+      <div className="flex items-center gap-4">
+        <div className="h-20 w-20 rounded-full bg-white border border-solid border-gray-200" />
+        <div>
+          <h1 className="text-2xl font-medium">{user.name}</h1>
+          <p className="leading-relaxed text-gray-600 text-sm">
+            On project since {formatDate({ createdAt: user.createdAt, pattern: 'dd MMM yyyy' })}
+          </p>
+          {user.bio ? <p className="leading-relaxed text-gray-600">{user.bio}</p> : null}
+        </div>
       </div>
       <button onClick={onEdit} className="border border-gray-200 border-solid w-full p-2 rounded-lg flex items-center justify-center">
         Edit
