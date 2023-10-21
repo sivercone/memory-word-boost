32962e864aba52c22523d21d5f4ff73c44769340
Danylo Trofimenko <49727045+sivercone@users.noreply.github.com>
2023-10-21 19:49:35 +0200

    fix(server): `logout` router

--- server/src/routes/authRoute.ts
+++ server/src/routes/authRoute.ts
@@ -14,7 +14,7 @@ class AuthRoute implements Routes {
   private initializeRoutes() {
     this.router.post(`${this.path}/login`, authController.logIn);
     this.router.get(`${this.path}/me`, isAuth, authController.me);
-    this.router.get(`${this.path}/logout`, isAuth, authController.logOut);
+    this.router.post(`${this.path}/logout`, isAuth, authController.logOut);
     this.router.get(`${this.path}/user/:id`, userController.getUserById);
     this.router.put(`${this.path}/user/update`, isAuth, userController.updateUser);
     this.router.delete(`${this.path}/user/:id`, isAuth, userController.deleteUser);
