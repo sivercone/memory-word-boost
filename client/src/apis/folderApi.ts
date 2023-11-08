0c21ae0b767b49e2eb5fe5d5990cc10aeeca6417
Danylo Trofimenko <49727045+sivercone@users.noreply.github.com>
2023-11-08 12:54:12 +0100

    refactor(apis): clean up

--- client/src/apis/folderApi.ts
+++ client/src/apis/folderApi.ts
@@ -1,25 +1,27 @@
 import http from '@src/lib/http';
 import { FolderInterface, FolderInterfaceDraft } from '@src/interfaces';
 import consts from '@src/lib/consts';
+import { setApi, authApi } from '@src/apis';
 
 export const folderApi = {
-  async get(excludeUserId: string | undefined): Promise<FolderInterface[]> {
+  async get(excludeUserId?: string): Promise<FolderInterface[]> {
     if (consts.isBackendLess) {
       await Promise.resolve();
-      return JSON.parse(localStorage[`${consts.storageKey}_folders`] || '[]');
+      const folders = JSON.parse(localStorage[`${consts.storageKey}_folders`] || '[]');
+      return Array.isArray(folders) ? folders : [];
     } else {
       const response = await http.get(`/folders${excludeUserId ? `?excludeUserId=${excludeUserId}` : ''}`);
       return response.data;
     }
   },
 
-  async getById(payload: string): Promise<FolderInterface> {
+  async getById(payload: string): Promise<FolderInterface | null> {
     if (consts.isBackendLess) {
       await Promise.resolve();
-      const sets = JSON.parse(localStorage[`${consts.storageKey}_sets`] || '[]');
-      const folders = JSON.parse(localStorage[`${consts.storageKey}_folders`] || '[]');
-      const folderById = Array.isArray(folders) ? folders.find((item) => item.id === payload) : null;
-      if (folderById) folderById.sets = Array.isArray(sets) ? sets.filter((item) => folderById.sets.includes(item.id)) : [];
+      const sets = await setApi.get();
+      const folders = await folderApi.get();
+      const folderById = folders.find((item) => item.id === payload) || null;
+      if (folderById) folderById.sets = sets.filter((item) => (folderById.sets as unknown as string[]).includes(item.id));
       return folderById;
     } else {
       const response = await http.get(`/folder/${payload}`);
@@ -30,8 +32,8 @@ export const folderApi = {
   async getByUser(id: string): Promise<FolderInterface[]> {
     if (consts.isBackendLess) {
       await Promise.resolve();
-      const folders = JSON.parse(localStorage[`${consts.storageKey}_folders`] || '[]');
-      const foldersByUser = Array.isArray(folders) ? folders.filter((item) => item.user.id === id) : [];
+      const folders = await folderApi.get();
+      const foldersByUser = folders.filter((item) => item.user.id === id);
       return foldersByUser;
     } else {
       const response = await http.get(`/folder/byuser/${id}`);
@@ -41,16 +43,17 @@ export const folderApi = {
 
   async save(data: FolderInterfaceDraft): Promise<string> {
     if (consts.isBackendLess) {
-      const folders = JSON.parse(localStorage[`${consts.storageKey}_folders`] || '[]');
-      const nextFolders = Array.isArray(folders) ? (data.id ? folders.filter((item) => item.id !== data.id) : folders) : [];
-      const currFolder = Array.isArray(folders) && data.id ? folders.find((item) => item.id === data.id) : {};
+      const folders = await folderApi.get();
+      const user = await authApi.me();
+      const nextFolders = folders.filter((item) => item.id !== data.id);
+      const currFolder = data.id ? folders.find((item) => item.id === data.id) : undefined;
       const saveFolder = {
         ...currFolder,
         ...data,
-        id: currFolder.id || crypto.randomUUID(),
-        sets: currFolder.sets || [],
-        user: JSON.parse(localStorage[`${consts.storageKey}_user`]),
-        createdAt: currFolder.id ? currFolder.createdAt : new Date().toISOString(),
+        id: currFolder?.id || crypto.randomUUID(),
+        sets: currFolder?.sets || [],
+        user: user,
+        createdAt: currFolder?.id ? currFolder.createdAt : new Date().toISOString(),
         updatedAt: new Date().toISOString(),
       } as FolderInterface;
       localStorage.setItem(`${consts.storageKey}_folders`, JSON.stringify([...nextFolders, saveFolder]));
@@ -64,8 +67,8 @@ export const folderApi = {
   async delete(id: string): Promise<void> {
     if (consts.isBackendLess) {
       await Promise.resolve();
-      const folders = JSON.parse(localStorage[`${consts.storageKey}_folders`]);
-      const nextFolders = Array.isArray(folders) ? folders.filter((item) => item.id !== id) : [];
+      const folders = await folderApi.get();
+      const nextFolders = folders.filter((item) => item.id !== id);
       localStorage.setItem(`${consts.storageKey}_folders`, JSON.stringify(nextFolders));
     } else await http.delete(`/folder/${id}`);
   },
--- client/src/apis/setApi.ts
+++ client/src/apis/setApi.ts
@@ -1,23 +1,25 @@
 import http from '@src/lib/http';
 import { SetInterface, SetInterfaceDraft } from '@src/interfaces';
 import consts from '@src/lib/consts';
+import { authApi, folderApi } from '@src/apis';
 
 export const setApi = {
-  async get(excludeUserId: string | undefined): Promise<SetInterface[]> {
+  async get(excludeUserId?: string): Promise<SetInterface[]> {
     if (consts.isBackendLess) {
       await Promise.resolve();
-      return JSON.parse(localStorage[`${consts.storageKey}_sets`]);
+      const sets = JSON.parse(localStorage[`${consts.storageKey}_sets`]);
+      return Array.isArray(sets) ? sets : [];
     } else {
       const response = await http.get(`/sets${excludeUserId ? `?excludeUserId=${excludeUserId}` : ''}`);
       return response.data;
     }
   },
 
-  async getById(payload: string): Promise<SetInterface> {
+  async getById(payload: string): Promise<SetInterface | null> {
     if (consts.isBackendLess) {
       await Promise.resolve();
-      const sets = JSON.parse(localStorage[`${consts.storageKey}_sets`] || '[]');
-      const setById = Array.isArray(sets) ? sets.find((item) => item.id === payload) : null;
+      const sets = await setApi.get();
+      const setById = sets.find((item) => item.id === payload) || null;
       return setById;
     } else {
       const response = await http.get(`/set/${payload}`);
@@ -28,8 +30,8 @@ export const setApi = {
   async getByUser(id: string): Promise<SetInterface[]> {
     if (consts.isBackendLess) {
       await Promise.resolve();
-      const sets = JSON.parse(localStorage[`${consts.storageKey}_sets`] || '[]');
-      const setsByUser = Array.isArray(sets) ? sets.filter((item) => item.user.id === id) : [];
+      const sets = await setApi.get();
+      const setsByUser = sets.filter((item) => item.user.id === id);
       return setsByUser;
     } else {
       const response = await http.get(`/set/byuser/${id}`);
@@ -40,18 +42,19 @@ export const setApi = {
   async save(data: SetInterfaceDraft): Promise<string> {
     if (consts.isBackendLess) {
       await Promise.resolve();
-      const folders = JSON.parse(localStorage[`${consts.storageKey}_folders`] || '[]');
-      const sets = JSON.parse(localStorage[`${consts.storageKey}_sets`] || '[]');
-      const nextSets = Array.isArray(sets) ? (data.id ? sets.filter((item) => item.id !== data.id) : sets) : [];
-      const currSet = Array.isArray(sets) && data.id ? sets.find((item) => item.id === data.id) : {};
+      const folders = await folderApi.get();
+      const sets = await setApi.get();
+      const user = await authApi.me();
+      const nextSets = sets.filter((item) => item.id !== data.id);
+      const currSet = data.id ? sets.find((item) => item.id === data.id) : undefined;
       const saveSet = {
         ...currSet,
         ...data,
-        id: currSet.id || crypto.randomUUID(),
+        id: currSet?.id || crypto.randomUUID(),
         cards: data.cards || [],
-        folder: (Array.isArray(folders) ? folders.find((item) => item.id === data.folderId) : currSet.folder) || null,
-        user: JSON.parse(localStorage[`${consts.storageKey}_user`]),
-        createdAt: currSet.id ? currSet.createdAt : new Date().toISOString(),
+        folder: folders.find((item) => item.id === data.folderId) || null,
+        user,
+        createdAt: currSet?.id ? currSet.createdAt : new Date().toISOString(),
         updatedAt: new Date().toISOString(),
       } as SetInterface & SetInterfaceDraft;
       delete saveSet.folderId;
@@ -72,7 +75,7 @@ export const setApi = {
   async delete(id: string): Promise<void> {
     if (consts.isBackendLess) {
       await Promise.resolve();
-      const sets = JSON.parse(localStorage[`${consts.storageKey}_sets`]);
+      const sets = await setApi.get();
       const nextSets = Array.isArray(sets) ? sets.filter((item) => item.id !== id) : [];
       localStorage.setItem(`${consts.storageKey}_sets`, JSON.stringify(nextSets));
     } else await http.delete(`/set/${id}`);
