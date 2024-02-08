1f2671b0c21c494399c8f7547cb4c057c5a65e6e
Danylo Trofimenko <49727045+sivercone@users.noreply.github.com>
2024-02-08 16:18:12 +0100

    test(Login): fields check

--- client/cypress/e2e/login.spec.cy.ts
+++ client/cypress/e2e/login.spec.cy.ts
@@ -1,7 +1,21 @@
+const mockValues = { email: 'user@example.com', password: '.password0123' };
+
 describe('login functionality', () => {
-  beforeEach(() => cy.visit('/login'));
+  beforeEach(() => {
+    cy.clearCookies();
+    cy.clearLocalStorage();
+    cy.visit('/login');
+  });
+
+  it('requires email and password', () => {
+    cy.get('[data-testid="button.submit"]').click();
+    cy.get('[data-testid="input.email"]').should('be.focused');
+    cy.get('[data-testid="input.email"]').type(mockValues.email);
+    cy.get('[data-testid="button.submit"]').click();
+    cy.get('[data-testid="input.password"]').should('be.focused');
+  });
+
   it('allows a user to log in successfully', () => {
-    const mockValues = { email: 'user@example.com', password: '.password0123' };
     cy.get('[data-testid="input.email"]').type(mockValues.email);
     cy.get('[data-testid="input.password"]').type(mockValues.password);
     cy.get('[data-testid="button.submit"]').click();
