2371d00c359b5d1ec753d0dff6fed15a0d433491
Danylo Trofimenko <49727045+sivercone@users.noreply.github.com>
2024-02-13 17:00:16 +0100

    fix: improvements with ui

--- client/src/modules/set/Flashcards/CompleteView.tsx
+++ client/src/modules/set/Flashcards/CompleteView.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ButtonSquare } from '@src/ui';
-import { BlurIcon, CheckCircleIcon, GrainIcon } from '@src/ui/Icons';
+import { BlurIcon, CheckCircleIcon, PackageIcon } from '@src/ui/Icons';
 
 const CompleteView: React.FC<{ queryId: string; cardsLength: number; scorePercentage: string }> = ({
   queryId,
@@ -9,15 +9,15 @@ const CompleteView: React.FC<{ queryId: string; cardsLength: number; scorePercen
 }) => {
   return (
     <div className="flex-1 flex flex-col gap-4 animate-fadeIn">
-      <div className="bg-violet-200 rounded-lg flex-1 flex flex-col justify-end gap-4 items-start p-8">
+      <div className="bg-violet-200 rounded-lg flex-1 flex flex-col justify-end gap-4 items-start p-4">
         <CheckCircleIcon height="48" width="48" className="fill-gray-800" />
         <p className="text-xl font-semibold">Well done!</p>
       </div>
-      <div className="bg-gray-200 rounded-lg flex-1 flex flex-col justify-end gap-4 items-start p-8">
-        <GrainIcon height="48" width="48" className="fill-gray-800" />
+      <div className="bg-gray-200 rounded-lg flex-1 flex flex-col justify-end gap-4 items-start p-4">
+        <PackageIcon height="48" width="48" className="fill-gray-800" />
         <p className="text-xl font-semibold">{`Cards Completed: ${cardsLength}`}</p>
       </div>
-      <div className="bg-gray-300 rounded-lg flex-1 flex flex-col justify-end gap-4 items-start p-8">
+      <div className="bg-gray-300 rounded-lg flex-1 flex flex-col justify-end gap-4 items-start p-4">
         <BlurIcon height="48" width="48" className="fill-gray-800" />
         <p className="text-xl font-semibold">{`Score: ${scorePercentage}%`}</p>
       </div>
