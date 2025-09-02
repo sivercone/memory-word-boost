c45dee0e04f2d9a6a80d4f85734f7ec76b64cf03
Danylo Trofimenko <49727045+sivercone@users.noreply.github.com>
2025-09-02 12:10:35 +0200

    fix: enhance utils

--- src/lib/utils/string.ts
+++ src/lib/utils/string.ts
@@ -1,7 +1,7 @@
 import { customAlphabet } from 'nanoid';
 
 export const trimExtraSpaces = (str: string): string => {
-  return str.replace(/^\s+|\s+$/g, '').replace(/\s+/g, ' ');
+  return str.trim().replace(/\s+/g, ' ');
 };
 
 export const nanoid = (alphabet = 'abcdefghijklmnopqrstuvwxyz1234567890', size = 21): string => {
